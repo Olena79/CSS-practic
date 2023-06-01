d139616d6184f38fb97cb343517df7d69054142f
Olena Klementieva <foksysmile@gmail.com>
2023-06-01 19:04:15 +0300

    lesson24

--- src/route/index.js
+++ src/route/index.js
@@ -445,6 +445,27 @@ router.get('/template-3', function (req, res) {
         href: 'https://example.com/',
         img: 'https://picsum.photos/140/140',
       },
+      {
+        title: 'Latest post',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        button: 'View details',
+        href: 'https://example.com/',
+        img: 'https://picsum.photos/140/140',
+      },
+      {
+        title: 'Featured story',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        button: 'View details',
+        href: 'https://example.com/',
+        img: 'https://picsum.photos/140/140',
+      },
+      {
+        title: 'Popular post',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        button: 'View details',
+        href: 'https://example.com/',
+        img: 'https://picsum.photos/140/140',
+      },
     ],
 
     form: {
